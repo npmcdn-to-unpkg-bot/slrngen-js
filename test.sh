1ea4df120d19513f0482e2ed9a3277f43b8654ca
Fernando Ortiz <fortiz2k@gmail.com>
2016-08-25 23:38:42 -0500

    change phantomjs to phantomjs-prebuilt

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ if [ ! -d "./node_modules/jasmine-node" ]; then
   npm install --save-dev jasmine-node@1
 fi
 if [ ! -d "./node_modules/phantomjs" ]; then
-  npm install --save-dev phantomjs@1.8.2-3
+  npm install --save-dev phantomjs-prebuilt@2.1.12
 fi
 
 ./node_modules/.bin/jasmine-node ./test
