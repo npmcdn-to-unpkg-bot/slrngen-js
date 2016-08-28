23da2c5678f2be3b316b16494a564eebb15b7f90
Fernando Ortiz <fortiz2k@gmail.com>
2016-08-27 20:54:39 -0500

    chore(build): change build process for minifying files

--- build.sh
+++ build.sh
@@ -8,8 +8,7 @@ if [ ! -d "./node_modules/jshint" ]; then
   npm install jshint@2
 fi
 
-
-./node_modules/.bin/uglifyjs sgen.js -c "evaluate=false" --comments "/ Copyright .*/" -m --source-map sgen-min.map -o sgen-min.js
 ./node_modules/.bin/jshint sgen.js
+./node_modules/.bin/uglifyjs sgen.js -c "evaluate=false" --comments "/ Copyright .*/" -m --source-map dist/sgen-min.map -o dist/sgen-min.js
 #./node_modules/.bin/docco sgen.js -o doc
 #cp -r doc/* ../gh-pages/sgen/doc
